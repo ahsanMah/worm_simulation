a6c556fdfc2d8dc8e0a442ef6c278b3979fb29fa
ahsanMah <mahmood.ahsan@outlook.com>
2016-07-12 15:49:46 -0400

    added header rows for parameter tables + updated interpolate script

--- inter_src.py
+++ inter_src.py
@@ -3,26 +3,38 @@ import csv
 def convert_to_float (item):
 	if item != '':
 		return float(item)
-	return None
+	return "N/A"
 
 def getNext (row_index, col_index, table):	
 	row_index += 1
 	while row_index < len(table):
-		if table[row_index][col_index] != None:
+		if table[row_index][col_index] != "N/A":
 			return (row_index,table[row_index][col_index])
 		row_index += 1
+
+'''
+Removes any empty spaces from the end of a row 
+'''
+def strip(row):
+	while row[-1] == '':
+		row = row[:-1]
+	return row
 
 def update_file (file_pathway):
 	data = open(file_pathway, "rU")
 	data_reader = csv.reader(data, dialect = "excel")
 	data_table = []
+	header_rows = []
 	index = 1
 	name = file_pathway[-12:]
 
+	header_rows.append(strip(data_reader.next())) #skips header row
+
 	if name == "ph-Table.csv":
-		data_table.append(data_reader.next()) #gets the number of experiment days from the top row
+		header_rows.append(strip(data_reader.next())) #skips second header row
 
 	for row in data_reader:
+		row = strip(row)
 		entry = map(convert_to_float,row) #converts all items to floats
 		data_table.append(entry)
 
@@ -31,8 +43,9 @@ def update_file (file_pathway):
 	#searches the table row by row and fills in any missing values
 	while index < len(data_table):
 		row = data_table[index]
+
 		for col_index,val in enumerate(row): #gets the index position and value respectively for each entry in the array
-			if val == None:
+			if val == "N/A":
 				prev_val = data_table[index - 1][col_index]
 				next_index,next_val = getNext(index,col_index,data_table) #gets next available data point
 				
@@ -42,6 +55,9 @@ def update_file (file_pathway):
 				
 	output = open(file_pathway, "wb")
 	data_writer = csv.writer(output)
+
+	for row in header_rows:
+		data_writer.writerow(row)
 
 	for row in data_table:
 		data_writer.writerow(row)
