9381f378e7c63b6c03064834315e78638840c53f
ahsanMah <mahmood.ahsan@outlook.com>
2016-07-15 11:36:38 -0400

    updated run script to remove existing output files

--- bs_run/run.sh
+++ bs_run/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm -R simulations/defaultRun/output/*
+rm -R simulations/roadTest/output/*
+
 java -Xmx2048m -Dfile.encoding=UTF-8 -cp dep/NetLogo.jar \
   org.nlogo.headless.Main \
   --model worms.nlogo \
